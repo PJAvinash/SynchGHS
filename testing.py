1c2afb68460c420f552df26d6772c6af774e2125
PJAvinash <avinashindian2.0@gmail.com>
2023-04-21 17:23:52 -0500

    testing

---
+++ testing.py
@@ -0,0 +1,19 @@
+import os
+
+# Create the logs directory if it doesn't exist
+if not os.path.exists('logs'):
+    os.makedirs('logs')
+
+with open('logs.txt', 'r') as f:
+    lines = f.readlines()
+
+# Loop through the lines and extract the lines starting with 'uid: 1'
+print(len(lines))
+uid1_lines = []
+for line in lines:
+    if line.__contains__('to--->:1'):
+        uid1_lines.append(line)
+
+# Write the lines starting with 'uid: 1' to a log file
+with open('logs/uid1.txt', 'w') as f:
+    f.writelines(uid1_lines)
